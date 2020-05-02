1ba20500ecdd527a8f6ecc858c9835688838ef62
54742586 <vijaybalajitheboss@gmail.com>
2020-05-03 01:48:05 +0530

    Added csgo items in data.py

--- data.py
+++ data.py
@@ -3,3 +3,12 @@ MW = " (Minimal Wear)"
 FT = " (Field-Tested)"
 WW = " (Well-Worn)"
 BS = " (Battle-Scarred)"
+
+csitems = ["AK-47","AUG","AWP","Bayonet","Bowie Knife","Butterfly Knife","Classic Knife","CZ75-Auto",
+            "Desert Eagle","Dual Berettas","Falchion Knife","FAMAS","Five-SeveN","Flip Knife","G3SG1","Galil AR",
+            "Glock-18","Gut Knife","Huntsman Knife","Karambit","M249","M4A1-S","M4A4","M9 Bayonet","MAC-10","MAG-7",
+            "MP5-SD","MP7","MP9","Navaja Knife","Negev","Nomad Knife","Nova","P2000","P250","P90","Paracord Knife",
+            "PP-Bizon","R8 Revolver","Sawed-Off","SCAR-20","SG 553","Shadow Daggers","Skeleton Knife","SSG 08",
+            "Stiletto Knife","Survival Knife","Talon Knife","Tec-9","UMP-45","Ursus Knife","USP-S","XM1014"]
+
+quality = ["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred"]
