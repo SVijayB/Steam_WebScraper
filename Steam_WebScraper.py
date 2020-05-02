36048bf096847b936c6f76b89aeaece0e2bda4ed
54742586 <vijaybalajitheboss@gmail.com>
2020-05-02 21:47:11 +0530

    Added data.py for better readability

--- Steam_WebScraper.py
+++ Steam_WebScraper.py
@@ -2,37 +2,31 @@ import urllib.request
 import json
 import urllib.error
 import enum
-
-FN = " (Factory New)"
-MW = " (Minimal Wear)"
-FT = " (Field-Tested)"
-WW = " (Well-Worn)"
-BS = " (Battle-Scarred)"
+from data import *
 
 class MarketItem:
-
     sucess = False
     lowest_price = ''
     name = ''
     volume = 0
 
 def GetMarketItem(name):
-    strdat = ''
+    strdata = ''
     Item = MarketItem()
     name = name.replace(' ', '+')
     try:
         url = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=%s" % name)
         data = json.loads(url.read().decode())
-        strdat = str(data)
+        strdata = str(data)
         Item.name = name.replace('+', ' ')
     except urllib.error.URLError as e:
         print ('ERROR: %s' % e.reason)
         return MarketItem()
-    if strdat.find("success': True") != -1:
+    if strdata.find("success': True") != -1:
         Item.sucess = True
-    if strdat.find('lowest_price') != -1:
+    if strdata.find('lowest_price') != -1:
         Item.lowest_price = data['lowest_price']
-    if strdat.find('volume') != -1:
+    if strdata.find('volume') != -1:
         Item.volume = data['volume']
     return Item
 
@@ -46,6 +40,5 @@ def PrintMarketItem(it, volume=False):
         print ('No valid price found!')
     if volume and len(it.volume) > 0:
         print ("Volume :" ,it.volume)
-
 
 PrintMarketItem(GetMarketItem('Nova | Polar Mesh' + FT), True)
---
+++ data.py
@@ -0,0 +1,5 @@
+FN = " (Factory New)"
+MW = " (Minimal Wear)"
+FT = " (Field-Tested)"
+WW = " (Well-Worn)"
+BS = " (Battle-Scarred)"
