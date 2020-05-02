376ee0bf9feaf416cb244083692334b14ff5c0c5
54742586 <vijaybalajitheboss@gmail.com>
2020-05-02 22:45:04 +0530

    Added Url for reference

--- Steam_WebScraper.py
+++ Steam_WebScraper.py
@@ -4,41 +4,46 @@ import urllib.error
 import enum
 from data import *
 
-class MarketItem:
+class MarketItem():
     sucess = False
-    lowest_price = ''
-    name = ''
+    lowest_price = ""
+    name = ""
     volume = 0
 
 def GetMarketItem(name):
-    strdata = ''
+    strdata = ""
     Item = MarketItem()
-    name = name.replace(' ', '+')
+    name = name.replace(" ", "+")
     try:
-        url = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=%s" % name)
+        url = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=%s" %name)
+        print("\nURL for reference: http://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=%s" %name)
         data = json.loads(url.read().decode())
         strdata = str(data)
-        Item.name = name.replace('+', ' ')
+        Item.name = name.replace("+", " ")
     except urllib.error.URLError as e:
-        print ('ERROR: %s' % e.reason)
+        print("ERROR: %s" % e.reason)
         return MarketItem()
-    if strdata.find("success': True") != -1:
+    if (strdata.find("success': True") != -1):
         Item.sucess = True
-    if strdata.find('lowest_price') != -1:
+    else:
+        return MarketItem()
+    if (strdata.find('lowest_price') != -1):
         Item.lowest_price = data['lowest_price']
-    if strdata.find('volume') != -1:
+    if (strdata.find('volume') != -1):
         Item.volume = data['volume']
     return Item
 
-
-def PrintMarketItem(it, volume=False):
-    if len(it.name) > 0:
-        print (it.name + ': ')
-    if len(it.lowest_price) > 0:
-        print ("Lowest Price : ",it.lowest_price)
+def PrintMarketItem(item):
+    print("\nData Collected : ")
+    if (len(item.name) > 0):
+        print(item.name + ": ")
+    if (len(item.lowest_price) > 0):
+        print("Lowest Price :",item.lowest_price)
     else:
-        print ('No valid price found!')
-    if volume and len(it.volume) > 0:
-        print ("Volume :" ,it.volume)
+        print("Item Missing, Check Item Name!")
+    if (int(item.volume) > 0):
+        print("Volume :" ,item.volume)
+    else:
+        print("Invalid Item. Please check Item name again.")
 
-PrintMarketItem(GetMarketItem('Nova | Polar Mesh' + FT), True)
+PrintMarketItem(GetMarketItem("Nova | Polar Mesh" + WW))
