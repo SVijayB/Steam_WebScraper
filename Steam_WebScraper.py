d9b33e5b42098c0e1f142176dc4acd14dfaff0c4
54742586 <vijaybalajitheboss@gmail.com>
2020-05-02 13:48:19 +0530

    Finds profit, bug fixes, better readability

--- Steam_WebScraper.py
+++ Steam_WebScraper.py
@@ -10,26 +10,39 @@ def check_price_Steam():
 	page = requests.get(URL,headers=headers)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	price = soup.find("span", class_="market_listing_price market_listing_price_with_fee")
-	price = price.text  
-	result = re.findall(r"[-+]?\d*\.\d+|\d+", price)
-	result = float(result[0])
-	print("Currently the least value of given Item is : ", result)
+	price = price.text.strip()  
+	if(price=="Sold!"):
+		time.sleep(10)
+		check_price_Steam()
+	temp = re.findall(r"[-+]?\d*\.\d+|\d+", price)
+	result = 0.0
+	result = ".".join(temp).strip()
+	result = float(result)
+	print("Presently the least value of given Item is :", price.strip())
 	return result
 
 
-URL = input("Enter the URL of the product you want to check the price of: ")
+URL = input("\nEnter the URL of the product you want to check the price of: ")
 current_price = check_price_Steam()
+print("Note : Convert given price to your currency and then enter minimum price.")
+print("Current value of item in decimals : ",current_price)
 min_price = float(input("Enter the minimum price below which you want to be notified: "))
 
 def main():
 	a = True
 	while(a==True):
 		if current_price < min_price:
-			print("We found a lesser price !")
+			print("We found your product at a lesser price!")
+			print("You will be saving : ", round(min_price - current_price, 2))
 			return
 		else:
 			print('Searching...')
-			time.sleep(20)
+			time.sleep(5)
+			check_price_Steam()
+			if current_price < min_price:
+				print("We found your product at a lesser price!")
+				print("You will be saving : ", round(min_price - current_price, 2))
+				return
 
 if __name__ == "__main__":
     main()
