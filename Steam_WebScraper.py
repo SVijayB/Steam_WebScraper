66e08a460809122f3297f7b9421f82451c389a60
54742586 <vijaybalajitheboss@gmail.com>
2020-05-02 21:23:28 +0530

    Avoided median price to get a more precise value

--- Steam_WebScraper.py
+++ Steam_WebScraper.py
@@ -3,17 +3,16 @@ import json
 import urllib.error
 import enum
 
-FN = ' (Factory New)'
-MW = ' (Minimal Wear)'
-FT = ' (Field-Tested)'
-WW = ' (Well-Worn)'
-BS = ' (Battle-Scarred)'
+FN = " (Factory New)"
+MW = " (Minimal Wear)"
+FT = " (Field-Tested)"
+WW = " (Well-Worn)"
+BS = " (Battle-Scarred)"
 
 class MarketItem:
 
     sucess = False
     lowest_price = ''
-    median_price = ''
     name = ''
     volume = 0
 
@@ -22,7 +21,7 @@ def GetMarketItem(name):
     Item = MarketItem()
     name = name.replace(' ', '+')
     try:
-        url = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=%s" % name)
+        url = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=%s" % name)
         data = json.loads(url.read().decode())
         strdat = str(data)
         Item.name = name.replace('+', ' ')
@@ -31,8 +30,6 @@ def GetMarketItem(name):
         return MarketItem()
     if strdat.find("success': True") != -1:
         Item.sucess = True
-    if strdat.find('median_price') != -1:
-        Item.median_price = data['median_price']
     if strdat.find('lowest_price') != -1:
         Item.lowest_price = data['lowest_price']
     if strdat.find('volume') != -1:
@@ -43,9 +40,7 @@ def GetMarketItem(name):
 def PrintMarketItem(it, volume=False):
     if len(it.name) > 0:
         print (it.name + ': ')
-    if len(it.median_price) > 0:
-        print (it.median_price)
-    elif len(it.lowest_price) > 0:
+    if len(it.lowest_price) > 0:
         print (it.lowest_price)
     else:
         print ('No valid price found!')
@@ -53,4 +48,4 @@ def PrintMarketItem(it, volume=False):
         print (it.volume)
 
 
-PrintMarketItem(GetMarketItem('AWP | PAW' + FN), True)
+PrintMarketItem(GetMarketItem('Nova | Polar Mesh' + FT), True)
