948142247851bbd00cdb0cfaed01c30ba0ac5f43
vijay <vijaybalajitheboss@gmail.com>
2020-05-29 19:55:22 +0530

    Excepted all run time errors

--- src/name.py
+++ src/name.py
@@ -1,21 +1,32 @@
 from Steam_WebScraper import *
 def name():
-	try:
-		for x in range(len(csitems)): 
-			print (x+1,csitems[x])
-		wep = input("Pick the number that matches the Item you are looking for : ")
-		wep = int(wep)-1
-		print("Selected Item :",csitems[wep])
-		for x in range(len(quality)): 
-			print (x+1,quality[x])
-		qual = input("Pick the Quality of skin you are looking for : ")
-		qual = int(qual)-1
-		print("Selected Quality :",quality[qual])
-		nm = input("Enter the name of the skin you are looking for(Has to match steam DataBase) : ")
-		final = csitems[wep]+" | "+nm+qualfinal[qual]
-		return final
-	except:
-		print("\nERROR : WRONG CHOICE")
-		print("Check Spelling and Pick the right Numbers!")
-		print("PROGRAM TERMINATED")
-		sys.exit(0)
+	for x in range(len(csitems)): 
+		print (x+1,csitems[x])
+		time.sleep(0.000025)
+	print("Pick the number that matches the Item you are looking for")
+	wep = 0
+	while (wep<1 or wep>53):
+		try:
+			wep = int(input("> "))
+			if(wep<1 or wep>53):
+				raise ValueError
+		except ValueError:
+			print("ERROR : INVALID NUMBER")
+	wep = int(wep)-1
+	print("\nSelected Item -",csitems[wep]+"\n")
+	for x in range(len(quality)): 
+		print (x+1,quality[x])
+	print("Pick the Quality of skin you are looking for")
+	qual = 0
+	while (qual<1 or qual>5):
+		try:
+			qual = int(input("> "))
+			if(qual<1 or qual>5):
+				raise ValueError
+		except ValueError:
+			print("ERROR : INVALID NUMBER")
+	qual = int(qual)-1
+	print("\nSelected Quality -",quality[qual]+"\n")
+	nm = input("Enter the name of the skin you are looking for(Has to match steam DataBase)\n> ")
+	final = csitems[wep]+" | "+nm+qualfinal[qual]
+	return final
