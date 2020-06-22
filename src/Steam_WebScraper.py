d1620a395d10f0daf4b83c460112eeb837814685
vijay <vijaybalajitheboss@gmail.com>
2020-06-22 14:03:29 +0530

    Added in a logo and colours module

--- src/Steam_WebScraper.py
+++ src/Steam_WebScraper.py
@@ -4,6 +4,9 @@ from Modules.MarketItem import MarketItem
 from Modules.MarketItem import *
 
 if __name__ == "__main__":
+	logo = open("../assets/logo.txt" , "r").read()
+	print(logo)
+	print("\n"+"-"*30)
 	data = open("../assets/version.txt" , "r").read()
 	print("Steam_WebScraper | " + data)
 	time.sleep(1)
